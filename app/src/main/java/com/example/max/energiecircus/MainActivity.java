ba991ded9599aad3f3b015717cba36a36680d36a
Jeanbverr <jeanbaptiste.verroken@gmail.com>
2016-10-05 14:41:43 +0200

    Registratie Frontend homescreen Frontend registratiescherm Registratie namen + klas

--- app/src/main/java/com/example/max/energiecircus/MainActivity.java
+++ app/src/main/java/com/example/max/energiecircus/MainActivity.java
@@ -1,7 +1,15 @@
 package com.example.max.energiecircus;
 
-import android.os.Bundle;
+import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
+
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Html;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,7 +18,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+    }
 
+    public void classRegistration(View v)
+    {
+        Intent showActivity = new Intent(this, RegistrationActivity.class);
+        startActivity(showActivity);
+
+    }
+
+    public void loadWebsite(View v){
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://thinkcore.be/project/caves-energieke-scholen"));
+        startActivity(browserIntent);
     }
 
 }
---
+++ app/src/main/java/com/example/max/energiecircus/registrationActivity.java
@@ -0,0 +1,74 @@
+package com.example.max.energiecircus;
+
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    SharedPreferences SharedPreferences;
+    EditText name;
+    EditText klas;
+    Button registratieKnop;
+    Editor editor;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+
+
+        name = (EditText) findViewById(R.id.name);
+        klas = (EditText) findViewById(R.id.klas);
+        registratieKnop = (Button) findViewById(R.id.registratie);
+
+        //File name and mode. Mode "0" is private mode
+        SharedPreferences = getApplicationContext().getSharedPreferences("RegistrationActivity",0);
+        editor = SharedPreferences.edit();
+
+    }
+
+    public void registreren(View v){
+
+        String nameTxt = name.getText().toString();
+        String klasTxt = klas.getText().toString();
+
+        if(name.getText().length()<=0){
+            Toast.makeText(RegistrationActivity.this, "Kies een naam", Toast.LENGTH_SHORT).show();
+        }
+        else if(klas.getText().length()<=0){
+            Toast.makeText(RegistrationActivity.this, "Kies een klas", Toast.LENGTH_SHORT).show();
+        }
+
+        editor.putString("Naam", nameTxt);
+        editor.putString("Klas", klasTxt);
+        editor.commit();
+
+        SharedPreferences prefs = getSharedPreferences("RegistrationActivity",0);
+        String naamRegistratie = prefs.getString("Naam",null);
+        String klasRegistratie = prefs.getString("Klas",null);
+        if(naamRegistratie !=null && klasRegistratie !=null){
+            Log.e("naam",naamRegistratie);
+            Log.e("klas",klasRegistratie);
+        }
+
+
+        Intent showActivity = new Intent(this, RegistrationActivity.class);
+        startActivity(showActivity);
+
+
+
+
+
+    }
+
+
+
+}
